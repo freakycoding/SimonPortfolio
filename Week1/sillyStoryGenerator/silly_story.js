6697c3c9d1661feb67bc336742c565c7f9d862dc
freakycoding <medicenyb@gmail.com>
2026-06-18 23:01:17 +0200

    update: added returnRandomStrory function

--- Week1/sillyStoryGenerator/silly_story.js
+++ Week1/sillyStoryGenerator/silly_story.js
@@ -5,30 +5,39 @@ const generateBtn = document.querySelector(".generate");
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
 }
 
 // Raw text strings
+const characters = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+];
 
-// the soup kitchen
-// Disneyland
-// the White House
+const places = [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"
+];
 
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+const events = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and slithered away"
+
+]
 
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+    const storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. 
+    When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. 
+    Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`
 
-  return storyText;
+    return storyText;
 }
 
 // Event listener and partial generate function definition
@@ -36,16 +45,18 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
-  if (customName.value !== "") {
-    const name = customName.value;
-  }
+    if (customName.value !== "") {
+        const name = customName.value;
+        const newStory = returnRandomStoryString();
+    }
 
-  if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
-  }
+    if (document.getElementById("uk").checked) {
+        const weight = Math.round(300);
+        const temperature = Math.round(94);
+    }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
-  story.style.visibility = "visible";
+    // TODO: replace "" with the correct expression
+    // story.textContent = "";
+    story.textContent = "";
+    story.style.visibility = "visible";
 }
